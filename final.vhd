a03d7988a6cec66df141f0fe4784eb963b442b98
sharlinak <sharlinak@users.noreply.github.com>
2014-12-08 13:47:35 -1000

    Update final.vhd

--- final.vhd
+++ final.vhd
@@ -10,7 +10,8 @@ ENTITY final IS
       LCD_RW                 : OUT STD_LOGIC;
       LCD_ON                 : OUT STD_LOGIC;
       LCD_DATA               : INOUT STD_LOGIC_VECTOR(7 DOWNTO 0);
-		HEX0, HEX1: OUT STD_LOGIC_VECTOR(0 TO 6)
+		HEX0, HEX1, HEX2, HEX3: OUT STD_LOGIC_VECTOR(0 TO 6);
+		IRDA_RXD: in std_logic
 	);
 END final;
 
@@ -51,26 +52,42 @@ ARCHITECTURE arch OF final IS
 				BLANK : IN STD_LOGIC); --blank flag
 	END COMPONENT;
 	
+	COMPONENT finalir 
+	port (
+     CLOCK_50: in std_logic;
+	  reset: in std_logic;
+	  IRDA_RXD: in std_logic;
+	  input_data: out std_logic_vector(7 DOWNTO 0));
+	end component;
 	
 	SIGNAL COUNTER : INTEGER RANGE 0 TO 40000000:= 0;
 	SIGNAL COUNTER2 : INTEGER RANGE 0 TO 10000000:= 0;
+	SIGNAL COUNTER3 : INTEGER RANGE 0 TO 10000000:= 0;
 	
-	SIGNAL SCORE : INTEGER RANGE 0 TO 31:= 0;
-	SIGNAL SCORE_VECTOR : STD_LOGIC_VECTOR (7 DOWNTO 0);
+	SIGNAL SCORE : INTEGER RANGE 0 TO 31 := 15;
+	SIGNAL ASYNCSCORE : INTEGER RANGE 0 TO 31 := 0;
+	SIGNAL SCORE_VECTOR : STD_LOGIC_VECTOR (7 DOWNTO 0) := "00000000";
 	
 	SIGNAL reset: STD_LOGIC; 
 	SIGNAL A0 : STD_LOGIC_VECTOR (1 DOWNTO 0);
 	SIGNAL A1 : STD_LOGIC_VECTOR (1 DOWNTO 0);
 	SIGNAL A2 : STD_LOGIC_VECTOR (1 DOWNTO 0);
 	SIGNAL A3 : STD_LOGIC_VECTOR (1 DOWNTO 0);
-	SIGNAL CLOCK :STD_LOGIC;
-	SIGNAL CLOCK2: STD_LOGIC;
+	SIGNAL current_input : STD_LOGIC_VECTOR (2 DOWNTO 0) := "100";
+	SIGNAL CLOCK, CLOCK2 :STD_LOGIC;
+
+	SIGNAL IRRESETER : STD_LOGIC := '0';
+
+	SIGNAL old_counter, change_counter : INTEGER RANGE 0 TO 10 := 0;
+	
+	SIGNAL G0,G1,G2,G3 : STD_LOGIC_VECTOR(1 DOWNTO 0);
+	
+	SIGNAL input_data : STD_LOGIC_VECTOR(7 DOWNTO 0) := "00000000";
 	
 	SIGNAL OFFSET : INTEGER RANGE 0 TO 3:= 0;
 	
 BEGIN
-	reset <= NOT KEY(1);
-	--CLOCK <= KEY(0);
+	reset <= KEY(1);
 	
 	PROCESS (SCORE)
 	BEGIN
@@ -121,9 +138,6 @@ BEGIN
 		END CASE;
 	END PROCESS;
 	
-	
-	--SCORE_VECTOR <= STD_LOGIC_VECTOR(TO_UNSIGNED(SCORE, 12));
-	
 	PROCESS (CLOCK_50) -- 1.25 Hz clock
 	BEGIN
 		IF (rising_edge(CLOCK_50)) THEN
@@ -133,7 +147,6 @@ BEGIN
 			ELSE
 				CLOCK <= '0';
 			END IF;
-			
 		END IF;		
 	END PROCESS;
 	
@@ -146,7 +159,18 @@ BEGIN
 			ELSE
 				CLOCK2 <= '0';
 			END IF;
-			
+		END IF;		
+	END PROCESS;
+	
+	PROCESS (CLOCK_50) -- 5 Hz clock
+	BEGIN
+		IF (rising_edge(CLOCK_50)) THEN
+			COUNTER3 <= COUNTER3 + 1;
+			IF (COUNTER3 <= 625000) THEN
+				IRRESETER <= '0';
+			ELSE
+				IRRESETER <= '1';
+			END IF;
 		END IF;		
 	END PROCESS;
 	
@@ -154,14 +178,64 @@ BEGIN
 	BEGIN
 		IF (rising_edge(CLOCK2)) THEN
 			OFFSET <= OFFSET + 1;
-			SCORE <= SCORE + 1;
+			SCORE <= ASYNCSCORE;
 		END IF;
-	
 	END PROCESS;
-		
+	
+	PROCESS (input_data)
+	BEGIN
+		CASE (input_data) IS
+			WHEN X"05" =>
+				current_input <= "000";
+			WHEN X"07" =>
+				current_input <= "010";
+			WHEN X"08" =>
+				current_input <= "001";
+			WHEN X"09" =>
+				current_input <= "011";
+			WHEN OTHERS =>
+				current_input <= "100";
+		END CASE;
+	END PROCESS;
+	
+	PROCESS (IRRESETER, SCORE, current_input, OFFSET, G3)
+	BEGIN
+		IF(IRRESETER = '1') THEN
+			IF(OFFSET = 3) THEN
+				IF(current_input(2) = '0' AND current_input(1 DOWNTO 0) = G3) THEN
+					ASYNCSCORE <= SCORE + 1;
+				ELSE
+					ASYNCSCORE <= SCORE - 1;
+				END IF;
+			ELSE
+				ASYNCSCORE <= SCORE;
+			END IF;
+		ELSE
+			ASYNCSCORE <= ASYNCSCORE;
+		END IF;
+	END PROCESS;
+	
+	PROCESS (OFFSET)
+	BEGIN
+		IF(OFFSET = 0)	THEN
+			G0 <= A0;
+			G1 <= A1;
+			G2 <= A2;
+			G3 <= A3;
+		ELSE
+			G0 <= G0;
+			G1 <= G1;
+			G2 <= G2;
+			G3 <= G3;
+		END IF;
+	END PROCESS;
 		
 	DISP0 : bcd7seg PORT MAP (SCORE_VECTOR(3 DOWNTO 0), HEX0, '0', '0');
 	DISP1 : bcd7seg PORT MAP (SCORE_VECTOR(7 DOWNTO 4), HEX1, '0', '0');	
+	DISP2 : bcd7seg PORT MAP (input_data(3 DOWNTO 0), HEX2, '0', '0');	
+	DISP3 : bcd7seg PORT MAP (input_data(7 DOWNTO 4), HEX3, '0', '0');	
+	
+	FINAILIRR : finalir PORT MAP (CLOCK_50, IRRESETER, IRDA_RXD, input_data);
 	
 	-- instantiate LCD controller
 	lcd : LCD_Display PORT MAP(KEY(0 DOWNTO 0),CLOCK_50,LCD_RS, LCD_EN,LCD_RW ,LCD_ON,LCD_DATA, A0, A1, A2, A3,OFFSET);
